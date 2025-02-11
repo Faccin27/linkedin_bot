fd32a58a654a7ad72e360c4b6dd0c328d4571e97
Guilherme Faccin <gfaccin27@gmail.com>
2025-02-11 01:29:49 -0300

    feat: create LinkedInConfig class to store configurations

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import os
 import logging
+from dataclasses import dataclass
 
 logging.basicConfig(
-    level=logging.DEBUG,  # Alterado para DEBUG para mais detalhes
+    level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
 
+@dataclass
+class LinkedInConfig:
+    email: str
+    password: str
+    search_term: str
+    max_connections_per_page: int = 9
+    timeout: int = 10
+    max_pages: int = 100
+
 def main():
-    logger.debug("Iniciando projeto LinkedIn Automation")
+    config = LinkedInConfig(
+        email=os.getenv("LINKEDIN_EMAIL"),
+        password=os.getenv("LINKEDIN_PASSWORD"),
+        search_term="full stack"
+    )
+    logger.info(f"Configuração carregada: {config}")
 
 if __name__ == "__main__":
     main()
